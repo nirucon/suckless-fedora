7916cfaa788f43336cd01de09a1e39f1f587e61f
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2023-01-14 19:00:12 +0100

    Create symbolic-links.sh

---
+++ symbolic-links.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# some "automation" for my suckless setup
+
+echo "create symbolic links..."
+ln -s /media/1TB-TWO/Music ~/Music/mp3 &
+ln -s /media/1TB-ONE/Nextcloud/Wallpapers ~/Pictures/Wallpapers &
+ln -s /media/1TB-ONE/Nextcloud/Screenshots ~/Pictures/Screenshots &
+ln -s /media/1TB-ONE/Nextcloud/ ~/Nextcloud &
+
+sleep 1
+
+echo "links to mp3, Wallpaers, Screenshots, Nextcloud = done!"
