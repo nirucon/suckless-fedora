e1be00e0a72e3b914cf98bd4375050a387883aa8
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2023-01-14 16:01:44 +0100

    Create fedora-suckless-base.sh

---
+++ fedora-suckless-base.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo dnf install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel xorg nitrogen picom alsa-utils imlib2-devel libXrandr-devel nerd-fonts-ttf xautolock scrot
